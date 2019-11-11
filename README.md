6b7b31c318c6977519c41d9cd93bf514118f97d8
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-11-10 22:55:38 -0500

    Completed part 1 (Prerequisites) and part 2 (Routing) of Laravel 6 tutorial.

---
+++ README.md
@@ -0,0 +1,1 @@
+# LaracastsTutorialL6
---
+++ resources/views/chocolate.blade.php
@@ -0,0 +1,2 @@
+<p>Do you like chocolate?</p>
+<img src="https://cdn11.bigcommerce.com/s-ham8sjk/images/stencil/1280x1280/products/276/836/Cacao_Chocolate_Chocolate_Liquor__1551729718_104.172.159.225__96045.1551729753.jpg?c=2&imbypass=on">
