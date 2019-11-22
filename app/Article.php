cd2082ad6113bbc5127ad286ba1bae33d3155683
Iris Glaze <iris@iriss-mbp.attlocal.net>
2019-11-22 17:59:13 -0500

    added method extension to model to take note of usefulness of the getRouteKeyName() model method.

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    /*
+    *   Alternatively, could return "slug" if you want to retrieve articles by "slug".
+    *   There would have to be a slug attribute.
+    */
+    public function getRouteKeyName() {
+        return parent::getRouteKeyName();
+    }
 }
