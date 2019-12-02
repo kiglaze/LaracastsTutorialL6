597e056c49bdc38bea42d798cf2ab25d54bc3c80
Iris Glaze <iris@Iriss-MBP.carolina.rr.com>
2019-12-01 22:21:53 -0500

    If no articles can show, show no articles text instead. Handles for situation when articles filtered by tag show no articles.

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,7 +4,7 @@
     <div id="wrapper">
         <div id="page" class="container">
             <div id="content">
-                @foreach($articles as $article)
+                @forelse($articles as $article)
                     <div class="title">
                         <h2>
 {{--                            <a href="/articles/{{ $article->id }}">{{ $article->title }}</a>--}}
@@ -14,7 +14,9 @@
                     </div>
                     <p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
                     <p>{{ $article->body }}</p>
-                @endforeach
+                @empty
+                    <p>No relevant articles yet.</p>
+                @endforelse
             </div>
         </div>
     </div>
