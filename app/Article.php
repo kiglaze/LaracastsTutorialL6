25ce50d8e96dd5946ec66b9576c7a30a451db17a
Iris Glaze <iris@Iriss-MBP.carolina.rr.com>
2019-12-01 21:00:10 -0500

    Added Tag model and factory. Added many-to-many relationship between Tags and Articles. This work included adding a table for article-tag relationships.

--- app/Article.php
+++ app/Article.php
@@ -20,4 +20,8 @@ class Article extends Model
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
 }
